f7e98d73e16ce3b72ec921856716cfb75c1cb9ee
Oyster Lee <oysterd3@gmail.com>
2024-02-28 15:25:58 +0800

    refactor: allow chat to be scrollable

--- src/ChatApp.tsx
+++ src/ChatApp.tsx
@@ -5,6 +5,8 @@ import MessageHistory from '@/components/MessageHistory.tsx';
 import { createBEM } from '@/utils';
 import './styles/index.scss';
 import Sidebar from '@/components/Sidebar.tsx';
+import { ScrollArea } from '@/components/ui/scroll-area.tsx';
+import { useEffect, useRef } from 'react';
 
 const bem = createBEM();
 
@@ -12,23 +14,38 @@ const ChatApp = () => {
   const { messages } = useOpenAIStore((state) => ({
     messages: state.messages,
   }));
+  const chatRef = useRef<HTMLDivElement>(null);
+
+  useEffect(() => {
+    if (chatRef.current) {
+      chatRef.current.scrollTop = chatRef.current.scrollHeight + 999;
+    }
+  }, [messages]);
+
+  useEffect(() => {
+    if (chatRef.current) {
+      chatRef.current.scrollTop = chatRef.current.scrollHeight + 999;
+    }
+  }, []);
 
   return (
     <div className={bem()}>
       <Sidebar />
       <main className={bem('main')}>
-        <MessageHistory>
-          {messages.map((message, index) =>
-            message.role === 'system' ? null : (
-              <Message
-                key={index}
-                position={message.role === 'user' ? 'sent' : 'received'}
-              >
-                {message.content}
-              </Message>
-            ),
-          )}
-        </MessageHistory>
+        <ScrollArea ref={chatRef}>
+          <MessageHistory>
+            {messages.map((message, index) =>
+              message.role === 'system' ? null : (
+                <Message
+                  key={index}
+                  position={message.role === 'user' ? 'sent' : 'received'}
+                >
+                  {message.content}
+                </Message>
+              ),
+            )}
+          </MessageHistory>
+        </ScrollArea>
         <MessageInput />
       </main>
     </div>
