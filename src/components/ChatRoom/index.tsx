0db110e76c88ae0d7476e81c61cd812a431e7d53
Oyster Lee <oysterd3@gmail.com>
2024-03-01 19:57:05 +0800

    refactor: chat room style

--- src/components/ChatRoom/index.tsx
+++ src/components/ChatRoom/index.tsx
@@ -2,6 +2,10 @@ import { useOpenAIStore } from '@/stores/openai.ts';
 import MessageInput from '@/components/ChatRoom/MessageInput.tsx';
 import MessageHistory from '@/components/ChatRoom/MessageHistory.tsx';
 import ChatRoomPrompt from '@/components/ChatRoom/ChatRoomPrompt.tsx';
+import { createBEM } from '@/utils';
+import '@/styles/components/chat-room.scss';
+
+const bem = createBEM('chat-room');
 
 const ChatRoom = () => {
   const currentViewing = useOpenAIStore((state) => state.currentViewing);
@@ -11,11 +15,11 @@ const ChatRoom = () => {
   }
 
   return (
-    <>
+    <div className={bem()}>
       <ChatRoomPrompt />
       <MessageHistory />
       <MessageInput />
-    </>
+    </div>
   );
 };
 
